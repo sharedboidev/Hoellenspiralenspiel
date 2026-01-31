7bb987c8ecfc9567115be88a1c26df8747072c01
Christian Nytra <ch.nytra.13@gmail.com>
2026-01-31 16:06:12 +0100

    random startanimation versucht, geht nix :C

--- Scripts/Units/Enemies/BlueBlob.cs
+++ Scripts/Units/Enemies/BlueBlob.cs
@@ -4,14 +4,27 @@ namespace Hoellenspiralenspiel.Scripts.Units.Enemies;
 
 public partial class BlueBlob : BaseEnemy
 {
+    protected override PackedScene AttackScene { get; }
+
     public override void _Ready()
     {
         base._Ready();
 
         ChasedPlayer = CurrentScene.GetNode<Player2D>("%Player 2D");
+
+        RandomizeAnimation();
     }
 
-    protected override PackedScene AttackScene { get; }
+    private void RandomizeAnimation()
+    {
+        AnimationTree.Active = false;
+
+        var animationPlayer = GetNode<AnimationPlayer>(nameof(AnimationPlayer));
+        animationPlayer.Play("run_down");
+        animationPlayer.Seek(GD.Randf() * animationPlayer.CurrentAnimationLength, true);
+
+        AnimationTree.Active = true;
+    }
 
     protected override void ExecuteAttack() { }
 }
