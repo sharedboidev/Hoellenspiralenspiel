1db0fff842a37df2c7f8873793ba6048be60560a
Christian Nytra <ch.nytra.13@gmail.com>
2026-02-04 18:29:04 +0100

    naming

--- Scripts/UI/TestPlane.cs
+++ Scripts/UI/TestPlane.cs
@@ -1,12 +1,14 @@
 using Godot;
 
+namespace Hoellenspiralenspiel.Scripts.UI;
+
 public partial class TestPlane : Node
 {
-    [Export] private AudioStreamPlayer2D BackgroundPlayer;
+    [Export] private AudioStreamPlayer2D backgroundPlayer;
 
     public override void _Ready()
     {
-        if (!BackgroundPlayer.IsPlaying())
-            BackgroundPlayer.Play();
+        if (!backgroundPlayer.IsPlaying())
+            backgroundPlayer.Play();
     }
 }
