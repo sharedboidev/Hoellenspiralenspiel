5f1c2f65906a308412c946302c5eb302aee64b36
Christian Nytra <ch.nytra.13@gmail.com>
2026-02-05 20:16:53 +0100

    rotationDEGREES!!!!

--- Scripts/Test/TweenTestScene.cs
+++ Scripts/Test/TweenTestScene.cs
@@ -11,7 +11,7 @@ public partial class TweenTestScene : PanelContainer
 
         tween.TweenProperty(this, "position", Position + Vector2.Right * 1000, 2);
         tween.TweenProperty(this, "scale", Scale * 3f, 2);
-        tween.TweenProperty(this, "rotation", 15f, 3);
+        tween.TweenProperty(this, "rotation_degrees", 1080, 1f);
 
         base._Ready();
     }
