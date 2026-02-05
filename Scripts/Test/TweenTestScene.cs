45904683038f28af541e853ee1491eebe9b425aa
Christian Nytra <ch.nytra.13@gmail.com>
2026-02-05 10:00:04 +0100

    tweens können was

---
+++ Scripts/Test/TweenTestScene.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace Hoellenspiralenspiel.Scripts.Test;
+
+public partial class TweenTestScene : PanelContainer
+{
+    public override void _Ready()
+    {
+        var tween = GetTree().CreateTween()
+                             .SetParallel();
+
+        tween.TweenProperty(this, "position", Position + Vector2.Right * 1000, 2);
+        tween.TweenProperty(this, "scale", Scale * 3f, 2);
+        tween.TweenProperty(this, "rotation", 15f, 3);
+
+        base._Ready();
+    }
+}
