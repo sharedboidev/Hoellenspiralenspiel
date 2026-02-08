e941bc287c066ab6ae828622379a717289f7b352
Tobias <tlemster@outlook.com>
2026-02-08 18:49:55 +0100

    beeb bop

--- Scripts/Abilities/Spells/FrostNova.cs
+++ Scripts/Abilities/Spells/FrostNova.cs
@@ -1,52 +1,61 @@
+using System.Threading.Tasks;
 using Godot;
-using System;
-using Hoellenspiralenspiel.Enums;
 using Hoellenspiralenspiel.Scripts.Abilities;
 using Hoellenspiralenspiel.Scripts.Abilities.Spells;
 using Hoellenspiralenspiel.Scripts.Extensions;
-using Hoellenspiralenspiel.Scripts.Models;
-using Hoellenspiralenspiel.Scripts.Units;
 using Hoellenspiralenspiel.Scripts.Units.Enemies;
 
 public partial class FrostNova : Area2D,
-								  ISpell
+								 ISpell
 {
-	// Called when the node enters the scene tree for the first time.
-	private bool           durationOver = false;
-	private FrostNovaSkill skill;
-	public override void _Ready()
+	private CircleShape2D circleShape = ResourceLoader.Load<CircleShape2D>("res://Resources/CircleShape.tres");
+
+	[Export] public CollisionShape2D CollisionShape;
+	private         float            expansionTime = 0.2f;
+	private         float            lifeTime;
+	private         float            radius = 1000f;
+	private         CircleShape2D    runtimeShape;
+	private         FrostNovaSkill   skill;
+	[Export] public Sprite2D         Sprite;
+	private         float            spriteBaseRadius;
+
+	public void Init(BaseSkill s, Vector2 globalPlayerPosition, Vector2 _)
 	{
-		BodyEntered += OnBodyEntered;
+		skill          = s as FrostNovaSkill;
+		GlobalPosition = globalPlayerPosition;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{		
-		if (durationOver)
-			QueueFree();
-		else
-			Scale += new Vector2(8f  * (float)delta, 8f * (float)delta);
+	public override async void _Ready()
+	{
+		runtimeShape     =  (CircleShape2D)circleShape.Duplicate();
+		BodyEntered      += OnBodyEntered;
+		spriteBaseRadius =  Sprite.Texture.GetWidth() / 2f;
+		_                =  ExpandAsync();
+	}
+
+	private async Task ExpandAsync()
+	{
+		const float tick       = 0.05f;
+		var         radiusStep = radius / (expansionTime / tick);
+
+		while (runtimeShape.Radius < radius)
+		{
+			runtimeShape.Radius = Mathf.Min(runtimeShape.Radius + radiusStep, radius);
+			Sprite.Scale        = Vector2.One * (runtimeShape.Radius / spriteBaseRadius);
+			CollisionShape.SetShape(runtimeShape);
+			await ToSignal(GetTree().CreateTimer(tick), SceneTreeTimer.SignalName.Timeout);
+		}
+
+		QueueFree();
 	}
 
 	private void OnBodyEntered(Node2D body)
 	{
-		if (body.IsInGroup("monsters") &&  body is BaseEnemy enemy)
+		if (body.IsInGroup("monsters") && body is BaseEnemy enemy)
 		{
-			var result       = skill.MakeRealDamage(enemy);
+			var result = skill.MakeRealDamage(enemy);
 			enemy.LifeCurrent -= (int)result.Value;
 			enemy.InstatiateFloatingCombatText(result, GetTree().CurrentScene, new Vector2(0, -60));
 		}
-		
-	}
-
-	public void _on_expand_duration_timer_timeout()
-	{
-		durationOver = true;
-	}
-
-	public void Init(BaseSkill s, Vector2 globalPlayerPosition, Vector2 _)
-	{
-		this.skill          = s as FrostNovaSkill;
-		this.GlobalPosition = globalPlayerPosition;
 	}
 }
