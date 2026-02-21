b44c08365b6641cf5fdf57ba269516aa5068ac06
Tobias <tlemster@outlook.com>
2026-02-21 23:21:36 +0100

    Tooltip angepasst und der rest? Nur gott weiß wieso die jetzt erst mitkommen

--- Scenes/Spells/LightningStrike.cs
+++ Scenes/Spells/LightningStrike.cs
@@ -3,20 +3,20 @@ using Godot;
 
 public partial class LightningStrike : Node2D
 {
-    public override void _Ready()
-    {
-        GetNode<AnimatedSprite2D>("AnimationLightning").Play();
-        GetNode<AnimatedSprite2D>("AnimationExplosion").Play();
-        Started?.Invoke(this, EventArgs.Empty);
-    }
+	public override void _Ready()
+	{
+		GetNode<AnimatedSprite2D>("AnimationLightning").Play();
+		GetNode<AnimatedSprite2D>("AnimationExplosion").Play();
+		Started?.Invoke(this, EventArgs.Empty);
+	}
 
-    public event EventHandler Finished;
-    public event EventHandler Started;
+	public event EventHandler Finished;
+	public event EventHandler Started;
 
-    public void PlaySound()
-    {
-        var sfx = GetNode<AudioStreamPlayer2D>("SFX");
-        sfx.Finished += () => Finished?.Invoke(this, EventArgs.Empty);
-        sfx.Play();
-    }
+	public void PlaySound()
+	{
+		var sfx = GetNode<AudioStreamPlayer2D>("SFX");
+		sfx.Finished += () => Finished?.Invoke(this, EventArgs.Empty);
+		sfx.Play();
+	}
 }
