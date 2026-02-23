5141c7190778261f03f1dc982df8a75f17690fc9
Christian Nytra <ch.nytra.13@gmail.com>
2026-02-23 18:12:15 +0100

    Lootsystem refactor und erweiterung um AffixLoading

--- Scripts/Controllers/Lootsystem.cs
+++ Scripts/Controllers/Lootsystem.cs
@@ -1,5 +1,8 @@
-﻿using Godot;
+﻿using System;
+using System.IO;
+using Godot;
 using Godot.Collections;
+using Hoellenspiralenspiel.Resources.Affixes;
 using Hoellenspiralenspiel.Scripts.Items;
 using Hoellenspiralenspiel.Scripts.Units.Enemies;
 using LootTable = Hoellenspiralenspiel.Resources.LootTable;
@@ -8,58 +11,84 @@ namespace Hoellenspiralenspiel.Scripts.Controllers;
 
 public partial class Lootsystem : Node
 {
-    private Dictionary<string, LootTable> Tables { get; set; } = new();
+    private Dictionary<string, LootTable> Tables  { get; set; } = new();
+    private Array<Affix>                  Affixes { get; set; } = new();
 
     [Export]
     public string LootTablesPath { get; set; } = "res://Resources/LootTables/";
+
+    [Export]
+    public string AffixesPath { get; set; } = "res://Resources/Affixes";
 
     public override void _Ready() => LoadAllTables();
 
     public BaseItem[] GenerateLoot(BaseEnemy enemy)
     {
-        if(!Tables.TryGetValue(enemy.LootTableId, out var fittingTable))
+        if (!Tables.TryGetValue(enemy.LootTableId, out var fittingTable))
             return [];
 
         var loot = fittingTable.RollLoot();
 
+        RollModifierFor(loot);
+
         return loot;
+    }
+
+    private void RollModifierFor(BaseItem[] loot)
+    {
+
     }
 
     private void LoadAllTables()
     {
-        LoadTablesFromDirectory(LootTablesPath);
+        LoadLootTables();
+        LoadAffixes();
+    }
+
+    private void LoadAffixes()
+    {
+        LoadResourcesFromDirectory(AffixesPath, LoadAffixIntoCollection);
+
+        GD.Print($"Loaded {Affixes.Count} Affixes");
+    }
+
+    private void LoadLootTables()
+    {
+        LoadResourcesFromDirectory(LootTablesPath, LoadTableIntoDictionary);
 
         GD.Print($"Loaded {Tables.Count} loot tables");
     }
 
-    private void LoadTablesFromDirectory(string path)
+    private void LoadResourcesFromDirectory(string path, Action<string> loadAction)
     {
         using var directory = DirAccess.Open(path);
-
-        if (directory == null)
-        {
-            GD.PushError($"Failed to open directory: {path}");
-            return;
-        }
-
-        var fileName = GetFileName(directory);
+        var fileName = GetDirectoryFilename(path, directory);
 
         while (!string.IsNullOrWhiteSpace(fileName))
         {
-            var fullPath = path + fileName;
+            var fullPath =Path.Combine(path, fileName);
 
             if (directory.CurrentIsDir())
             {
                 if (fileName != "." && fileName != "..")
-                    LoadTablesFromDirectory(fullPath + "/");
+                    LoadResourcesFromDirectory(fullPath + "/", loadAction);
             }
             else if (fileName.EndsWith(".tres") || fileName.EndsWith(".res"))
-                LoadTableIntoDictionary(fullPath);
+                loadAction(fullPath);
 
             fileName = directory.GetNext();
         }
 
         directory.ListDirEnd();
+    }
+
+    private static string GetDirectoryFilename(string path, DirAccess directory)
+    {
+        if (directory == null)
+            throw new IOException($"Failed to open directory: {path}");
+
+        var fileName = GetFileName(directory);
+        return fileName;
     }
 
     private void LoadTableIntoDictionary(string fullPath)
@@ -72,6 +101,18 @@ public partial class Lootsystem : Node
 
             GD.Print($"Loaded loot table: {table.TableId}");
         }
+    }
+
+    private void LoadAffixIntoCollection(string fullPath)
+    {
+        var affix = GD.Load<Affix>(fullPath);
+
+        if(affix is null)
+            return;
+
+        Affixes.Add(affix);
+
+        GD.Print($"Loaded Affix: {affix.ResourceName}");
     }
 
     private static string GetFileName(DirAccess directory)
