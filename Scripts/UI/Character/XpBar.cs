db498bc96e813c89fc6d055e73295ab7a6fd44d4
Christian Nytra <ch.nytra.13@gmail.com>
2026-03-12 22:20:47 +0100

    hardcoded xp table

--- Scripts/UI/Character/XpBar.cs
+++ Scripts/UI/Character/XpBar.cs
@@ -1,20 +1,48 @@
 using Godot;
+using Hoellenspiralenspiel.Scripts.Units;
 
 namespace Hoellenspiralenspiel.Scripts.UI.Character;
 
 public partial class XpBar : Control
 {
     private TextureProgressBar xpBar;
+    private Label              xpDisplayLabel;
 
     public override void _Ready()
     {
-        xpBar = GetNode<TextureProgressBar>("%Bar");
+        LoadNodes();
+        SetPositionInViewport();
+    }
 
+    private void SetPositionInViewport()
+    {
         var viewportSize = GetViewportRect().Size;
 
         var xPositionBar = (viewportSize.X - xpBar.Size.X * Scale.X) / 2;
         var yPositionBar = viewportSize.Y - xpBar.Size.Y * Scale.Y - 10;
 
-        GlobalPosition = new Vector2(xPositionBar, yPositionBar);
+        Position = new Vector2(xPositionBar, yPositionBar);
     }
+
+    public void SetMaxValue(Player2D player)
+    {
+        
+    }
+
+    public void SetCurrentValue(Player2D player)
+    {
+        
+    }
+
+    private void LoadNodes()
+    {
+        xpBar          = GetNode<TextureProgressBar>("%Bar");
+        xpDisplayLabel = GetNode<Label>("%XpDisplay");
+    }
+
+    public void _on_bar_mouse_exited()
+        => xpDisplayLabel.Visible = false;
+
+    public void _on_bar_mouse_entered()
+        => xpDisplayLabel.Visible = true;
 }
