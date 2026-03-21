1b906ae97006f2bb619d5a1bbf02117f7a76e4fa
Christian Nytra <ch.nytra.13@gmail.com>
2026-03-21 17:19:37 +0100

    3d movement OKK

--- Scripts/Units/Player.cs
+++ Scripts/Units/Player.cs
@@ -15,10 +15,25 @@ public partial class Player : CharacterBody3D
     public override void _Process(double delta)
     {
         base._Process(delta);
-
+        HandleMovementInputs();
         ResolveInvincibility(delta);
     }
 
+    [Export]
+    public Vector3 MovementDirection { get; set; }
+
+    public float Movementspeed { get; set; } = 100;
+
+    private void HandleMovementInputs()
+    {
+        var kek = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+
+        MovementDirection = new Vector3(kek.X, 0, kek.Y);
+
+        Velocity          = MovementDirection * Movementspeed;
+
+        MoveAndSlide();
+    }
     private void ResolveInvincibility(double delta)
     {
         if (millisecondsSinceLastHit > InvicibilityTimeMilliseconds)
